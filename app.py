3bb60b98a9ee2665e30ce6c98963f984599e0e3b
URIELSENTINEL <130925095+URIELSENTINEL@users.noreply.github.com>
2023-04-22 20:25:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,12 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/ARTHUR', methods=['GET', 'POST'])
+@app.route('/ARTHUR')
 def funcao():
-    # Aqui você pode definir sua função em Python
-    response = make_response("Essa é uma função em Python")
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+    return 'página arthur'
 
 if __name__ == '__main__':
     app.run(debug=True)
