033ff6ff344cb73fbe00e51b03073a01bd91c472
URIELSENTINEL <130925095+URIELSENTINEL@users.noreply.github.com>
2023-04-21 21:10:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']
+    if query.lower() == 'google':
+        return redirect('https://www.google.com')
+    else:
+        return 'Pesquisa não suportada'
 
 if __name__ == '__main__':
     app.run()
