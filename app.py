91737b7c3d4eb3766d992ae690e8956141840ab4
URIELSENTINEL <130925095+URIELSENTINEL@users.noreply.github.com>
2023-04-21 21:31:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     render_template('index.html')
-
-@app.route('/', methods=['GET', 'POST'])
-def get_text():
-    if request.method == 'POST':
-        text = request.form['text']
-        if text.lower() == 'google':
-            return redirect('https://www.google.com')
-        else:
-            return 'Texto não suportado'
-
 
 if __name__ == '__main__':
     app.run()
