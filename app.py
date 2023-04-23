1734c20f610d1aedc90b960cb9cdade174f07047
URIELSENTINEL <130925095+URIELSENTINEL@users.noreply.github.com>
2023-04-22 21:19:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/')
+@app.route('/URIEL/ARTHUR/')
 def funcao():
     return render_template('arthur.html')
 
