b6331f5941c50355fc5bda9a7da9090a914315be
URIELSENTINEL <130925095+URIELSENTINEL@users.noreply.github.com>
2023-04-22 21:12:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/ARTHUR/')
+@app.route('/')
 def funcao():
-    return render_template('index.html')
+    return render_template('arthur.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
