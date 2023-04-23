badf451ae439180c3d7ce2475d8e3afa1fddb630
URIELSENTINEL <130925095+URIELSENTINEL@users.noreply.github.com>
2023-04-22 21:03:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/arthur')
+@app.route('/ARTHUR/')
 def funcao():
     return render_template('index.html')
 
